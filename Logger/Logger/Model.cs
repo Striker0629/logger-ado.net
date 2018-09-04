565d8c12a23fee080def4484e10318f9b895c60e
Striker0629 <37600398+Striker0629@users.noreply.github.com>
2018-09-04 20:48:01 +0300

    first1.2
    add model

--- Logger/Logger/Model.cs
+++ Logger/Logger/Model.cs
@@ -1,16 +1,32 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data.Sql;
+using System.Data.SqlClient;
 
 namespace Logger
 {
     class Model
     {
-        public Model()
+        private SqlConnection connection;
+        public Model(string init="master",bool integr=true)
+        {
+            connection = new SqlConnection(GetConnectionString(init, integr));
+
+        }
+
+        public void InsertData()
         {
 
+        }
+
+        public void ReadData()
+        {
+
+        }
+
+        static String GetConnectionString(string initial,bool integrated)
+        {
+            String  connect=System.Configuration.ConfigurationManager.ConnectionStrings["Connect"].ConnectionString;
+            return String.Format("Data Source={0};Initial Catalog={1};Integrated Security={2};", connect, initial, integrated);
         }
 
 
