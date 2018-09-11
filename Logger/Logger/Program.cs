54afba176382931be684ba75b67235834c8df2ac
Striker0629 <37600398+Striker0629@users.noreply.github.com>
2018-09-11 18:22:56 +0300

    Test1.1.1

--- Logger/Logger/Program.cs
+++ Logger/Logger/Program.cs
@@ -13,19 +13,26 @@ namespace Logger
         {
             model = new Model();
         }
+        static Program()
+        {
+            login = "login";
+            logout = "logout";
+        }
         static void Main(string[] args)
         {
             Program program = new Program();
             if (args.Contains(login))
             {
+                Console.WriteLine("Login");
                 program.model.InsertDataLogin();
             }
             else if (args.Contains(logout))
             {
-
+                Console.WriteLine("Logout");
             }
             else
                 throw new ArgumentException("Неверный параметр коммандной строки");
+            Console.ReadKey();
         }
 
 
